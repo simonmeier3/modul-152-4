d2c65697c234b8ae3f86c19b48be31c85601e4d9
Simon Meier <meier-simon@gmx.ch>
2021-04-10 16:55:52 +0200

    Started build media Player

--- server.ts
+++ server.ts
@@ -2,6 +2,8 @@ import express = require('express');
 import multer = require('multer');
 
 const app = express();
+let audioFile;
+let vttFile;
 
 let store = multer.diskStorage({
     destination: (req, file, callback) =>{
@@ -16,8 +18,8 @@ let upload = multer({storage: store});
 
 app.post('/api/audio', upload.fields([{name: 'audio', maxCount: 1}, {name: 'vtt', maxCount: 1}]), (req, res) => {
 
-    let audioFile = req.files['audio'][0].filename;
-    let vttFile = req.files['vtt'][0].filename;
+    audioFile = req.files['audio'][0].filename;
+    vttFile = req.files['vtt'][0].filename;
 
     res.json({
         data: {
